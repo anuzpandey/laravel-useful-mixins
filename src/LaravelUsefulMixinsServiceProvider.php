99c0466d340f2b0b6a271f5ba31bc0492bbce3b3
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2024-05-28 22:28:15 +0545

    :recycle: refactor: Remove Command Files and Registration.

--- src/LaravelUsefulMixinsServiceProvider.php
+++ src/LaravelUsefulMixinsServiceProvider.php
@@ -10,16 +10,10 @@ class LaravelUsefulMixinsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-useful-mixins')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_laravel-useful-mixins_table')
-            ->hasCommand(LaravelUsefulMixinsCommand::class);
+            ->hasMigration('create_laravel-useful-mixins_table');
     }
 }
